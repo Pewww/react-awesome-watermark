48a9fbb227a3712bfbf0ef4d5d6692628177a87d
pewww <jung22337@naver.com>
2021-05-02 20:42:04 +0900

    :zap: Translate canvas position to center when multiple option is given

--- src/lib/canvasToImage.ts
+++ src/lib/canvasToImage.ts
@@ -32,12 +32,19 @@ const canvasToImage = ({
   ctx.font = `${fontSize}px ${fontFamily}`;
   ctx.fillStyle = color;
   ctx.globalAlpha = opacity;
-  ctx.rotate((Math.PI / 180) * rotate);
 
   if (multiple) {
     const textWidth = ctx.measureText(text).width;
     const horizontalSpace = Math.floor(textWidth * 2);
     const verticalSpace = Math.floor(fontSize * 2.5);
+
+    const centerX = Math.floor(width / 2);
+    const centerY = Math.floor(height / 2);
+
+    // Reference: https://riptutorial.com/html5-canvas/example/19532/rotate-an-image-or-path-around-it-s-centerpoint?fbclid=IwAR0D-71k4SVAG4B8swwGkJecCqenDSOnO78ScLhoJYsoYqE5BWutAfXnoIo
+    ctx.translate(centerX, centerY);
+    ctx.rotate((Math.PI / 180) * rotate);
+    ctx.translate(-centerX, -centerY);
 
     for (let posX = 0; posX <= width + textWidth; posX += horizontalSpace) {
       for (
@@ -48,11 +55,10 @@ const canvasToImage = ({
         ctx.fillText(text, posX, posY);
       }
     }
-
-    // @TODO: Translate context position
   } else {
     const { x, y } = position!;
 
+    ctx.rotate((Math.PI / 180) * rotate);
     ctx.fillText(text, x, y);
   }
 
